7e0a5c391eef78370a81345ace2e02b74eeb7104
Suleyman Zahi <suleyman.zahi1@gmail.com>
2020-07-10 21:14:25 +0200

    First commit

---
+++ contacts_ui.py
@@ -0,0 +1,49 @@
+
+import time
+
+class Contact:
+    def __init__(self, first_name, last_name, number):
+        self.fname = first_name
+        self.lname = last_name
+        self.nbr = number
+
+def create_contact():
+    fname = input("Enter first name: ")
+    lname = input("Enter last name: ")
+    tretto = input("Enter number: ")
+    return Contact(fname, lname, tretto)
+
+
+menu = """
+Choose one the alternatives below:
+1. Add new contact
+2. Search for contact
+3. Show all contacts
+4. Remove a contact
+4. Exit application
+"""
+contacts_list = []
+program = True
+
+while program:
+    
+    print(menu)
+    choice = input("Choose option: ")
+    if choice == "1":
+        c = create_contact()
+        print(f'Contact {c.fname} {c.lname} with number {c.nbr} successfully created.')
+        contacts_list.append(c)
+        time.sleep(2)
+    elif choice == "3":
+        for c in contacts_list:
+            print(contacts_list.index(c) + 1)
+            print(f"Name: {c.fname} {c.lname}")
+            print(f"Number: {c.nbr}")
+            print("--------------------")
+    elif choice == "4":
+        program = False
+        print("Exiting application...")
+        time.sleep(1)
+    else:
+        print("Input failed. Try again")
+
